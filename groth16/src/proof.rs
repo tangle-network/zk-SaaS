71854290d161abe26b7085e849926dfd2c56fce9
Thomas Braun <braunth@oregonstate.edu>
2023-09-29 12:23:29 -0400

    generalize F1 and F2

--- groth16/src/proof.rs
+++ groth16/src/proof.rs
@@ -1,4 +1,6 @@
 #![allow(non_snake_case)]
+
+use std::ops::{AddAssign, Mul};
 use ark_ec::pairing::Pairing;
 use ark_ff::{FftField, PrimeField};
 use ark_groth16::Proof;
@@ -27,17 +29,18 @@ fn calculate_A<
 }
 
 fn calculate_B<
-    F: FftField + PrimeField + Into<u64>,
-    E: Pairing<G2Affine = F>,
+    F1: FftField + PrimeField + Into<u64>,
+    F2: FftField + PrimeField + Into<u64>,
+    E: Pairing<G2Affine = F2>,
 >(
-    Z: F,
-    K: F,
-    s: F,
-    V: Vec<F>,
-    a: Vec<F>,
+    Z: F2,
+    K: F2,
+    s: F2,
+    V: Vec<F2>,
+    a: Vec<F1>,
 ) -> E::G2Affine {
     // Start out by calculating the product of V_i^a_i
-    let mut prod = F::one();
+    let mut prod = F2::one();
     for i in 0..V.len() {
         prod *= V[i].pow(&[a[i].into()]);
     }
@@ -49,27 +52,30 @@ fn calculate_B<
     lhs * prod
 }
 
-fn calculate_h_of_x<F: FftField + PrimeField>(
-    a: Vec<F>,
-    u_x: Vec<F>,
-    v_x: Vec<F>,
-    w_x: Vec<F>,
-    t_of_x_secret_shares: Vec<F>,
-) -> Vec<F> {
+fn calculate_h_of_x<
+    F1: FftField + PrimeField + Mul<F2> + AddAssign<<F1 as Mul<F2>>::Output>,
+    F2: FftField + PrimeField,
+>(
+    a: Vec<F1>,
+    u_x: Vec<F1>,
+    v_x: Vec<F2>,
+    w_x: Vec<F1>,
+    t_of_x_secret_shares: Vec<F1>,
+) -> Vec<F1> {
     // Calculate the sum of a*u_x
-    let mut sum_u = F::zero();
+    let mut sum_u = F1::zero();
     for i in 0..a.len() {
         sum_u += a[i] * u_x[i];
     }
 
     // Calculate the sum of a*v_x
-    let mut sum_v = F::zero();
+    let mut sum_v = F1::zero();
     for i in 0..a.len() {
         sum_v += a[i] * v_x[i];
     }
 
     // Calculate the sum of a*w_x
-    let mut sum_w = F::zero();
+    let mut sum_w = F1::zero();
     for i in 0..a.len() {
         sum_w += a[i] * w_x[i];
     }
@@ -87,33 +93,34 @@ fn calculate_h_of_x<F: FftField + PrimeField>(
 }
 
 fn calculate_C<
-    F: FftField + PrimeField + Into<u64>,
-    E: Pairing<G1Affine = F>,
+    F1: FftField + PrimeField + Into<u64>,
+    F2: FftField + PrimeField + Into<u64>,
+    E: Pairing<G1Affine = F1>,
 >(
-    W: Vec<F>,
-    a: Vec<F>,
-    U: Vec<F>,
-    h: Vec<F>,
-    A: F,
-    s: F,
-    M: F,
-    r: F,
-    H: Vec<F>,
+    W: Vec<F1>,
+    a: Vec<F1>,
+    U: Vec<F1>,
+    h: Vec<F1>,
+    A: F1,
+    s: F2,
+    M: F1,
+    r: F1,
+    H: Vec<F1>,
 ) -> E::G1Affine {
     // Calculate the product of W_i^a_i
-    let mut prod = F::one();
+    let mut prod = F1::one();
     for i in 0..W.len() {
         prod *= W[i].pow(&[a[i].into()]);
     }
 
     // Calculate the product of U_i^h_i
-    let mut prod2 = F::one();
+    let mut prod2 = F1::one();
     for i in 0..U.len() {
         prod2 *= U[i].pow(&[h[i].into()]);
     }
 
     // Calculate the product of H_i^a_i
-    let mut prod3 = F::one();
+    let mut prod3 = F1::one();
     for i in 0..H.len() {
         prod3 *= H[i].pow(&[a[i].into()]);
     }
@@ -138,27 +145,28 @@ fn calculate_C<
 }
 
 fn calculate_groth16_proof<
-    F: FftField + PrimeField + Into<u64>,
-    E: Pairing<G1Affine = F, G2Affine = F>,
+    F1: FftField + PrimeField + Into<u64> + Mul<F2> + AddAssign<<F1 as Mul<F2>>::Output>,
+    F2: FftField + PrimeField + Into<u64>,
+    E: Pairing<G1Affine = F1, G2Affine = F2>,
 >(
-    L: F,
-    N: F,
-    r: F,
-    S: Vec<F>,
-    a: Vec<F>,
-    Z: F,
-    K: F,
-    s: F,
-    V: Vec<F>,
-    W: Vec<F>,
-    U: Vec<F>,
-    h: Vec<F>,
-    A: F,
-    M: F,
-    t_of_x_secret_shares: Vec<F>,
+    L: F1,
+    N: F1,
+    r: F1,
+    S: Vec<F1>,
+    a: Vec<F1>,
+    Z: F2,
+    K: F2,
+    s: F2,
+    V: Vec<F2>,
+    W: Vec<F1>,
+    U: Vec<F1>,
+    h: Vec<F1>,
+    A: F1,
+    M: F1,
+    t_of_x_secret_shares: Vec<F1>,
 ) -> Proof<E> {
-    let a_proof = calculate_A::<F, E>(L, N, r, S, a.clone());
-    let b_proof = calculate_B::<F, E>(Z, K, s, V.clone(), a.clone());
+    let a_proof = calculate_A::<F1, E>(L, N, r, S, a.clone());
+    let b_proof = calculate_B::<F1, F2, E>(Z, K, s, V.clone(), a.clone());
 
     let h_of_x_values = calculate_h_of_x(
         a.clone(),
@@ -168,7 +176,7 @@ fn calculate_groth16_proof<
         t_of_x_secret_shares,
     );
 
-    let c_proof = calculate_C::<F, E>(W, a, U, h, A, s, M, r, h_of_x_values);
+    let c_proof = calculate_C::<F1, F2, E>(W, a, U, h, A, s, M, r, h_of_x_values);
 
     Proof::<E> {
         a: a_proof,
