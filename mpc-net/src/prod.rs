d95a33d4ada9f68c934dc07b4d92dd83486e47b8
Thomas Braun <braunth@oregonstate.edu>
2023-10-26 10:49:20 -0400

    Allow generic io

--- mpc-net/src/prod.rs
+++ mpc-net/src/prod.rs
@@ -11,7 +11,7 @@ use rustls::{RootCertStore, ServerConfig};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::net::{TcpStream, ToSocketAddrs};
 use tokio::sync::Mutex;
 use tokio_rustls::{TlsAcceptor, TlsStream};
@@ -45,7 +45,7 @@ pub fn create_server_mutual_tls_acceptor<T: CertToDer>(
     let client_auth = AllowAnyAuthenticatedClient::new(client_certs);
     let server_config = ServerConfig::builder()
         .with_safe_defaults()
-        .with_client_cert_verifier(Arc::new(client_auth))
+        .with_client_cert_verifier(client_auth.boxed())
         .with_single_cert(
             vec![rustls::Certificate(
                 server_certificate.serialize_certificate_to_der()?,
@@ -77,11 +77,33 @@ pub fn create_client_mutual_tls_connector<T: CertToDer>(
     Ok(tokio_rustls::TlsConnector::from(Arc::new(client_config)))
 }
 
-pub struct ProdNet {
+pub trait HasPeerAddr {
+    fn peer_addr(&self) -> Result<SocketAddr, MpcNetError>;
+}
+
+impl HasPeerAddr for TlsStream<TcpStream> {
+    fn peer_addr(&self) -> Result<SocketAddr, MpcNetError> {
+        self.get_ref()
+            .0
+            .peer_addr()
+            .map_err(|err| MpcNetError::Generic(err.to_string()))
+    }
+}
+
+pub trait IOStream:
+    AsyncWrite + AsyncRead + HasPeerAddr + Unpin + Send + 'static
+{
+}
+impl<T: AsyncWrite + AsyncRead + HasPeerAddr + Unpin + Send + 'static> IOStream
+    for T
+{
+}
+
+pub struct ProdNet<T: IOStream> {
     /// The king will have a connection to each party, and each party will have a connection to the king.
     /// Thus, if this node is a king, there will be n_parties connections below. If this node is not a king,
     /// then, where will be only a single connection to the thing with ID 0
-    connections: MpcNetConnection<TlsStream<TcpStream>>,
+    connections: MpcNetConnection<T>,
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq)]
@@ -91,77 +113,39 @@ pub enum ProtocolPacket {
     Packet(Vec<u8>),
 }
 
-impl ProdNet {
+impl ProdNet<TlsStream<TcpStream>> {
     /// Returns when all the parties have connected.
-    pub async fn new_king<T: ToSocketAddrs, R: CertToDer>(
-        bind_addr: T,
+    pub async fn new_king_tls<V: ToSocketAddrs, R: CertToDer>(
+        bind_addr: V,
         identity: R,
-        client_certs: RootCertStore,
-    ) -> Result<Self, MpcNetError> {
+        root_cert_store: RootCertStore,
+    ) -> Result<ProdNet<TlsStream<TcpStream>>, MpcNetError> {
         let tcp_listener = tokio::net::TcpListener::bind(bind_addr).await?;
-        let n_peers = client_certs.len();
-        let tls_acceptor =
-            create_server_mutual_tls_acceptor(client_certs, identity)?;
+        let n_peers = root_cert_store.roots.len();
 
-        let mut connections = MpcNetConnection {
-            id: 0,
-            listener: None,
-            peers: Default::default(),
-            n_parties: n_peers + 1,
-        };
+        let tls_acceptor =
+            create_server_mutual_tls_acceptor(root_cert_store, identity)?;
+
+        let mut tls_conns = vec![];
 
         for _ in 0..n_peers {
             let (stream, _) = tcp_listener.accept().await?;
-            let peer_addr = stream.peer_addr()?;
-            let mut stream =
-                TlsStream::Server(tls_acceptor.accept(stream).await?);
-            let peer_id = stream.read_u32().await?;
-            // Now, multiplex the stream
-            let muxed =
-                multiplex_stream(MULTIPLEXED_STREAMS, true, stream).await?;
-            connections.peers.insert(
-                peer_id,
-                Peer {
-                    id: peer_id,
-                    listen_addr: peer_addr,
-                    streams: Some(muxed),
-                },
-            );
+            let stream = TlsStream::Server(tls_acceptor.accept(stream).await?);
+            tls_conns.push(stream);
         }
 
-        // Broadcast to each peer a SYN packet
-        for conn in connections.peers.values_mut() {
-            send_packet(
-                conn.streams.as_ref(),
-                MultiplexedStreamID::Zero,
-                ProtocolPacket::Syn,
-            )
-            .await?;
-        }
+        let n_parties = n_peers + 1;
 
-        // Wait for n_parties count of SynAck packets
-        for conn in connections.peers.values_mut() {
-            let packet =
-                recv_packet(conn.streams.as_ref(), MultiplexedStreamID::Zero)
-                    .await?;
-            if packet != ProtocolPacket::SynAck {
-                return Err(MpcNetError::Protocol {
-                    err: "Did not receive SynAck".to_string(),
-                    party: conn.id,
-                });
-            }
-        }
-
-        Ok(Self { connections })
+        ProdNet::new_from_pre_existing_connection(0, n_parties, tls_conns).await
     }
 
-    pub async fn new_peer<R: CertToDer, T: std::net::ToSocketAddrs>(
+    pub async fn new_peer_tls<R: CertToDer, V: std::net::ToSocketAddrs>(
         id: u32,
-        king: T,
+        king: V,
         identity: R,
         server_cert: RootCertStore,
         n_parties: usize,
-    ) -> Result<Self, MpcNetError> {
+    ) -> Result<ProdNet<TlsStream<TcpStream>>, MpcNetError> {
         let king_addr: SocketAddr =
             king.to_socket_addrs()?
                 .next()
@@ -172,56 +156,133 @@ impl ProdNet {
         let stream = TcpStream::connect(king_addr).await?;
         let tls_connector =
             create_client_mutual_tls_connector(server_cert, identity)?;
-        let mut stream = TlsStream::Client(
+        let stream = TlsStream::Client(
             tls_connector
                 .connect(rustls::ServerName::IpAddress(king_addr.ip()), stream)
                 .await?,
         );
-        stream.write_u32(id).await?;
-        let muxed =
-            multiplex_stream(MULTIPLEXED_STREAMS, false, stream).await?;
+
+        ProdNet::new_from_pre_existing_connection(id, n_parties, vec![stream])
+            .await
+    }
+}
+
+impl<T: IOStream> ProdNet<T> {
+    /// Must pass a list of connections to all the peers if king, otherwise a single connection
+    /// if a peer
+    pub async fn new_from_pre_existing_connection(
+        id: u32,
+        n_parties: usize,
+        mut ios: Vec<T>,
+    ) -> Result<Self, MpcNetError> {
+        if id != 0 {
+            if ios.len() != 1 {
+                return Err(MpcNetError::BadInput {
+                    err: "Must pass a single connection to the king if you are a peer",
+                });
+            }
+        }
+
         let mut connections = MpcNetConnection {
             id,
             listener: None,
             peers: Default::default(),
             n_parties,
         };
-        connections.peers.insert(
-            0,
-            Peer {
-                id: 0,
-                listen_addr: king_addr,
-                streams: Some(muxed),
-            },
-        );
 
-        // Wait for a Syn packet
-        let packet = recv_packet(
-            connections.peers.get(&0).unwrap().streams.as_ref(),
-            MultiplexedStreamID::Zero,
-        )
-        .await?;
-        if packet != ProtocolPacket::Syn {
-            return Err(MpcNetError::Protocol {
-                err: "Did not receive Syn".to_string(),
-                party: 0,
-            });
+        if id == 0 {
+            for mut stream in ios.into_iter() {
+                let peer_id = stream.read_u32().await?;
+                let peer_addr = stream.peer_addr()?;
+                let muxed =
+                    multiplex_stream(MULTIPLEXED_STREAMS, true, stream).await?;
+                connections.peers.insert(
+                    peer_id,
+                    Peer {
+                        id: peer_id,
+                        listen_addr: peer_addr,
+                        streams: Some(muxed),
+                    },
+                );
+            }
+        } else {
+            let mut stream = ios.pop().expect("Should exist");
+            let oeer_addr = stream.peer_addr()?;
+            stream.write_u32(id).await?;
+            let muxed =
+                multiplex_stream(MULTIPLEXED_STREAMS, false, stream).await?;
+            connections.peers.insert(
+                0,
+                Peer {
+                    id: 0,
+                    listen_addr: oeer_addr,
+                    streams: Some(muxed),
+                },
+            );
         }
 
-        // Send a SynAck packet to party_id=0
-        send_packet(
-            connections.peers.get(&0).unwrap().streams.as_ref(),
-            MultiplexedStreamID::Zero,
-            ProtocolPacket::SynAck,
-        )
-        .await?;
+        let this = Self { connections };
+        this.synchronize().await?;
 
-        Ok(Self { connections })
+        Ok(this)
+    }
+
+    /// Ensure all peers are connected to the king
+    async fn synchronize(&self) -> Result<(), MpcNetError> {
+        if self.is_king() {
+            // Broadcast to each peer a SYN packet
+            for conn in self.connections.peers.values() {
+                send_packet(
+                    conn.streams.as_ref(),
+                    MultiplexedStreamID::Zero,
+                    ProtocolPacket::Syn,
+                )
+                .await?;
+            }
+
+            // Wait for n_parties count of SynAck packets
+            for conn in self.connections.peers.values() {
+                let packet = recv_packet(
+                    conn.streams.as_ref(),
+                    MultiplexedStreamID::Zero,
+                )
+                .await?;
+                if packet != ProtocolPacket::SynAck {
+                    return Err(MpcNetError::Protocol {
+                        err: "Did not receive SynAck".to_string(),
+                        party: conn.id,
+                    });
+                }
+            }
+        } else {
+            // Wait for a Syn packet
+            let packet = recv_packet(
+                self.connections.peers.get(&0).unwrap().streams.as_ref(),
+                MultiplexedStreamID::Zero,
+            )
+            .await?;
+            if packet != ProtocolPacket::Syn {
+                return Err(MpcNetError::Protocol {
+                    err: "Did not receive Syn".to_string(),
+                    party: 0,
+                });
+            }
+
+            // Send a SynAck packet to party_id=0
+            send_packet(
+                self.connections.peers.get(&0).unwrap().streams.as_ref(),
+                MultiplexedStreamID::Zero,
+                ProtocolPacket::SynAck,
+            )
+            .await?;
+        }
+
+        Ok(())
     }
 }
 
 #[async_trait]
-impl MpcNet for ProdNet {
+impl<T: IOStream> MpcNet for ProdNet<T> {
     fn n_parties(&self) -> usize {
         self.connections.n_parties()
     }
@@ -255,8 +316,8 @@ impl MpcNet for ProdNet {
     }
 }
 
-async fn send_packet(
-    streams: Option<&Vec<Mutex<WrappedMuxStream<TlsStream<TcpStream>>>>>,
+async fn send_packet<T: IOStream>(
+    streams: Option<&Vec<Mutex<WrappedMuxStream<T>>>>,
     sid: MultiplexedStreamID,
     packet: ProtocolPacket,
 ) -> Result<(), MpcNetError> {
@@ -267,8 +328,8 @@ async fn send_packet(
     Ok(())
 }
 
-async fn recv_packet(
-    streams: Option<&Vec<Mutex<WrappedMuxStream<TlsStream<TcpStream>>>>>,
+async fn recv_packet<T: IOStream>(
+    streams: Option<&Vec<Mutex<WrappedMuxStream<T>>>>,
     sid: MultiplexedStreamID,
 ) -> Result<ProtocolPacket, MpcNetError> {
     let stream = streams.ok_or(MpcNetError::NotConnected)?;
@@ -289,10 +350,15 @@ mod test {
     use futures::stream::{FuturesOrdered, FuturesUnordered};
     use futures::{TryFutureExt, TryStreamExt};
     use std::future::Future;
+    use std::io::Error;
+    use std::pin::Pin;
+    use std::str::FromStr;
+    use std::task::{Context, Poll};
     use std::time::Duration;
     use tokio::net::TcpListener;
 
     use rcgen::{Certificate, RcgenError};
+    use tokio::io::ReadBuf;
 
     impl CertToDer for Certificate {
         fn serialize_certificate_to_der(&self) -> Result<Vec<u8>, MpcNetError> {
@@ -307,17 +373,17 @@ mod test {
         rcgen::generate_simple_self_signed(vec!["127.0.0.1".to_string()])
     }
 
-    struct LocalTestNetProd {
-        nodes: Vec<ProdNet>,
+    struct LocalTestNetProd<T: IOStream> {
+        nodes: Vec<ProdNet<T>>,
     }
 
-    impl LocalTestNetProd {
+    impl<T: IOStream> LocalTestNetProd<T> {
         pub async fn simulate_network_round<
             F: Future<Output = K> + Send,
             K: Send + Sync + 'static,
         >(
             self,
-            f: impl Fn(ProdNet) -> F + Send + Sync + Clone + 'static,
+            f: impl Fn(ProdNet<T>) -> F + Send + Sync + Clone + 'static,
         ) -> Vec<K> {
             let mut futures = FuturesOrdered::new();
             for connections in self.nodes.into_iter() {
@@ -332,6 +398,63 @@ mod test {
         }
     }
 
+    struct ChannelIO {
+        tx: tokio::sync::mpsc::UnboundedSender<Vec<u8>>,
+        rx: tokio::sync::mpsc::UnboundedReceiver<Vec<u8>>,
+    }
+
+    impl HasPeerAddr for ChannelIO {
+        fn peer_addr(&self) -> Result<SocketAddr, MpcNetError> {
+            Ok(SocketAddr::from_str("127.0.0.1:12345").unwrap())
+        }
+    }
+
+    impl AsyncWrite for ChannelIO {
+        fn poll_write(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+            buf: &[u8],
+        ) -> Poll<Result<usize, Error>> {
+            let len = buf.len();
+            self.tx.send(buf.into()).unwrap();
+            Poll::Ready(Ok(len))
+        }
+
+        fn poll_flush(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Result<(), Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn poll_shutdown(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Result<(), Error>> {
+            Poll::Ready(Ok(()))
+        }
+    }
+
+    impl AsyncRead for ChannelIO {
+        fn poll_read(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &mut ReadBuf<'_>,
+        ) -> Poll<std::io::Result<()>> {
+            match self.as_mut().rx.poll_recv(cx) {
+                Poll::Ready(Some(bytes)) => {
+                    buf.put_slice(&bytes);
+                    Poll::Ready(Ok(()))
+                }
+                Poll::Ready(None) => Poll::Ready(Err(Error::new(
+                    std::io::ErrorKind::Other,
+                    "Channel closed",
+                ))),
+                Poll::Pending => Poll::Pending,
+            }
+        }
+    }
+
     #[tokio::test(flavor = "multi_thread")]
     async fn test_network_init() {
         let _ = init_network(3).await;
@@ -343,9 +466,16 @@ mod test {
         let nodes = init_network(N_PEERS).await;
         let testnet = LocalTestNetProd { nodes };
         let expected_result: u32 = (0..=N_PEERS).map(|r| r as u32).sum();
+        add_protocol_inner(testnet, expected_result, N_PEERS).await;
+    }
 
+    async fn add_protocol_inner<T: IOStream>(
+        testnet: LocalTestNetProd<T>,
+        expected_result: u32,
+        n_peers: usize,
+    ) {
         let sums = testnet
-            .simulate_network_round(|net| async move {
+            .simulate_network_round(move |net| async move {
                 let my_id = net.party_id();
                 let bytes = bincode2::serialize(&my_id).unwrap();
                 if let Some(king_recv) = net
@@ -361,11 +491,12 @@ mod test {
                     let mut sum = 0;
                     for bytes in king_recv {
                         let id: u32 = bincode2::deserialize(&bytes).unwrap();
+                        println!("King recv ID: {}", id);
                         sum += id;
                     }
                     // now, send the sum to each of the clients
                     let bytes = bincode2::serialize(&sum).unwrap();
-                    let send = (0..(N_PEERS + 1))
+                    let send = (0..(n_peers + 1))
                         .map(|_| bytes.clone().into())
                         .collect::<Vec<Bytes>>();
                     net.client_receive_or_king_send(
@@ -395,7 +526,9 @@ mod test {
         assert!(sums.iter().all(|sum| *sum == expected_result));
     }
 
-    async fn init_network(n_peers: usize) -> Vec<ProdNet> {
+    async fn init_network(
+        n_peers: usize,
+    ) -> Vec<ProdNet<TlsStream<TcpStream>>> {
         let king_addr = TcpListener::bind("127.0.0.1:0")
             .await
             .unwrap()
@@ -428,7 +561,7 @@ mod test {
             client_identities.push(peer_identity);
         }
 
-        let king = tokio::spawn(ProdNet::new_king(
+        let king = tokio::spawn(ProdNet::<TlsStream<TcpStream>>::new_king_tls(
             king_addr,
             server_identity.clone(),
             client_certs.clone(),
@@ -439,7 +572,7 @@ mod test {
 
         let peers = FuturesUnordered::new();
         for (i, identity) in client_identities.into_iter().enumerate() {
-            let peer = ProdNet::new_peer(
+            let peer = ProdNet::new_peer_tls(
                 (i + 1) as u32,
                 king_addr,
                 identity,
